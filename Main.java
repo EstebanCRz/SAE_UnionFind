6fe15d178c0fad21e47b9d16c344c67360919c8d
Esteban <esteban.cardaillac@gmail.com>
2023-10-04 18:24:47 +0200

    .

--- Main.java
+++ Main.java
@@ -4,54 +4,104 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean ok = true;
-        int ami1;
-        int ami2;
+        boolean BonNum = true;
+        String fonctionName = null;
+        String ami1 = null;
+        String ami2 = null;
+        int nbparam = 0;
         int population = 10;
         UnionFind uf = new UnionFind(population);
 
-        System.out.println("Pour ajouter une personne :\"ajouter\"");
-        System.out.println("Pour lier des amis :\"lier valeurs1 valeurs2\"");
-        System.out.println("Pour isoler une personne :\"isoler valeurs1\"");
-        System.out.println("Pour savoir si ils sont amis :\"ami valeurs1 valeurs2\"");
-        System.out.println("Pour quitter :\"q\"");
+        System.out.println("Pour ajouter une personne :    \"ajouter\"");
+        System.out.println("Pour lier des amis :           \"lier valeurs1 valeurs2\"");
+        System.out.println("Pour isoler une personne :     \"isoler valeurs1\"");
+        System.out.println("Pour savoir si ils sont amis : \"ami valeurs1 valeurs2\"");
+        System.out.println("Pour quitter :                 \"q\"");
 
         Scanner scan = new Scanner(System.in);
 
+        while (ok) {
+            String line = scan.nextLine();
+            String[] parts = line.split(" ");
 
-        while(ok) {
-            String fonctionName = scan.next();
-            //recupere la fonction
+            if (parts.length > 0) {
+                fonctionName = parts[0];
+                nbparam = parts.length - 1;
+                if (nbparam >= 1) {
+                    ami1 = parts[1];
+                    if (!isInteger(ami1)) {
+                        BonNum = false;
+                    }
+                }
+                if (nbparam == 2) {
+                    ami2 = parts[2];
+                    if (!isInteger(ami2)) {
+                        BonNum = false;
+                    }
+                }
+            }
 
-            switch (fonctionName) {
-                case "lier":
-                    ami1 = Integer.parseInt(scan.next());
-                    ami2 = Integer.parseInt(scan.next());
-                    uf.union(ami1, ami2);
-                    break;
+            if (BonNum){
+                switch (fonctionName) {
+                    case "lier":
+                        if (nbparam==2) {
+                            uf.union(Integer.parseInt(ami1), Integer.parseInt(ami2));
+                        }
+                        else BonNum = false;
+                        break;
 
-                case "isoler":
-                    ami1 = Integer.parseInt(scan.next());
-                    uf.isolate(ami1);
-                    break;
+                    case "isoler":
+                        if (nbparam==1) {
+                            uf.isolate(Integer.parseInt(ami1));
+                        }
+                        else BonNum = false;
+                        break;
 
-                case "ami":
-                    ami1 = Integer.parseInt(scan.next());
-                    ami2 = Integer.parseInt(scan.next());
-                    uf.areFriends(ami1, ami2);
-                    break;
+                    case "ami":
+                        if (nbparam==2) {
+                            uf.areFriends(Integer.parseInt(ami1), Integer.parseInt(ami2));
+                        }
+                        else BonNum = false;
+                        break;
 
-                case "ajouter":
-                    uf.addElement();
-                    break;
+                    case "ajouter":
+                        if (nbparam==2) {
+                            uf.addElement();
+                            population++;
+                        }
+                        else BonNum = false;
+                        break;
 
-                case "q":
-                    ok = false;
-                    break;
+                    case "q":
+                        if (nbparam==0) {
+                            System.out.println("Au Revoir");
+                            ok = false;
+                        }
+                        break;
 
-                default:
-                    System.out.println("vous avez mal écrit");
-                    break;
+                    default:
+                        System.out.println("vous avez mal écrit la fonction");
+                }
+            }
+            if (!BonNum) {
+                System.out.println("vous avez mal écrit les paramètres");
+                BonNum = true;
+                scan.nextLine();
             }
         }
     }
+
+
+    private static boolean isInteger(String input) {
+        //Pass in string
+        try { //Try to make the input into an integer
+            Integer.parseInt( input );
+            return true; //Return true if it works
+        }
+        catch( Exception e ) {
+            return false; //If it doesn't work return false
+        }
+    }
 }
+
+
