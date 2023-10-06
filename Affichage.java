ad47b87b49f5260620d7c1dda163e1929bee5639
Esteban <esteban.cardaillac@gmail.com>
2023-10-06 14:52:35 +0200

    finit

--- Affichage.java
+++ Affichage.java
@@ -9,14 +9,6 @@ public class Affichage {
     public static void erreur_fonction() {
         System.out.println("vous avez mal écrit les paramètres");
     }
-
-    public void erreur_emplacement2() {
-        System.out.println("Eh oh y'a quelqu'un ici");
-    }
-
-    public void erreur_emplacement3() {
-        System.out.println("Eh oh y'a quelqu'un ici");
-    }
     static void regle() {
         System.out.println("Pour ajouter une personne :    \"ajouter\"");
         System.out.println("Pour lier des amis :           \"lier valeurs1 valeurs2\"");
